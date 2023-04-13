aa0da3a935513180e73d9787fa4450df4c17df27
mr.White <WhiteRA_@mail.ru>
2023-04-13 16:33:55 +1000

    Задача 12

--- main.py
+++ main.py
@@ -38,3 +38,16 @@ while i <= q:
     if i>=q:
         break
     print(i)
+
+
+S = int(input())
+P = int(input())
+i = 0
+j = 0
+while i < S:
+    while j < P:
+        if i + j == S and i * j == P:
+            break
+        j+=1
+    i+=1
+print(int(i/2), int(j/2))
