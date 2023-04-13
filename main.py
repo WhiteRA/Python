a8ae0412d429e736e9739fe9882a2870063fb3b4
mr.White <WhiteRA_@mail.ru>
2023-04-13 17:39:39 +1000

    Задача 10

--- main.py
+++ main.py
@@ -51,3 +51,16 @@ while i < S:
         j+=1
     i+=1
 print(int(i/2), int(j/2))
+
+
+import random
+n = int(input())
+k = 0
+for i in range(n):
+    v = int(random.randint(0, 1))
+    if v == 1:
+        k += 1
+if k < n / 2:
+    print(k)
+else:
+    print(n-k)
