cb8a4e6e0cfe150711c91931b32f9b7626d0c190
mr.White <WhiteRA_@mail.ru>
2023-04-13 11:45:51 +1000

    Задача 6

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ print ((int(a / 100))+(int((a % 100) / 10))+(int(a % 10)))
 
 a = int(input())
 print((a//6), ((a//6)*4), (a//6))
+
+a = int(385967)
+q = int(a % 10)
+w = int(a % 100 /10)
+e = int(a % 1000 /100)
+z = q + w + e
+f = int(a // 100000)
+v = int(a // 10000) % 10
+g = int(a // 1000) % 10
+n = f + v + g
+if z == n:
+    print('YES')
+else:
+    print('NO')
