29ddaeb3ffe381de46cb6f80eeb5a08922897945
mr.White <WhiteRA_@mail.ru>
2023-04-13 12:02:44 +1000

    Задача 8

--- main.py
+++ main.py
@@ -17,3 +17,11 @@ if z == n:
     print('YES')
 else:
     print('NO')
+
+n = int(input())
+m = int(input())
+k = int(input())
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print('YES')
+else:
+    print('NO')
