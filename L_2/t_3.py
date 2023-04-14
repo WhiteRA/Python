b54b2f803f743e157c9477a2536bbbe939acf2e9
Aleksandr Parhomenko <whitera_@mail.ru>
2023-04-14 13:15:52 +1000

    Dobavil komentarii

--- L_2/t_3.py
+++ L_2/t_3.py
@@ -1,9 +1,10 @@
+# Использую ручное возведение в степень вместо **
 q = int(input("Степень - "))
 a = int(input('Число - '))
 i = 1
 print(1)
 while i <= q:
     i *= a
-    if i>=q:
+    if i >= q:
         break
     print(i)
