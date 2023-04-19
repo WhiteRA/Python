301b1adf5f5b90c89e780eaa79fa7f2b73afd0d4
Aleksandr Parhomenko <whitera_@mail.ru>
2023-04-19 13:34:52 +1000

    L_3 - t_1

---
+++ L_3/t_1.py
@@ -0,0 +1,11 @@
+list_0 = []
+q = int(input("Число элемнтов - "))
+w = int(input("Искомый элемент - "))
+for i in range(q):
+    list_0.append(i+1)
+print(list_0)
+x = 0
+for l in range(q):
+    if l == w:
+        x += 1
+print(f"Это число встречается {x} раз")
