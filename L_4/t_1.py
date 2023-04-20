47c7e18c30504fbb74071cfbf36a3c4b6ef03a6a
Aleksandr Parhomenko <whitera_@mail.ru>
2023-04-20 16:28:08 +1000

    Начало 4ого ДЗ

---
+++ L_4/t_1.py
@@ -0,0 +1,13 @@
+n = int(input("Ввод числа N - "))
+m = int(input("Ввод числа M - "))
+list_0 = set()
+list_1 = set()
+for i in range(n):
+    list_0.add(int(input("Ввод числа первого множества - ")))
+print(list_0)
+print()
+
+for j in range(m):
+    list_1.add(int(input("Ввод числа второго множества - ")))
+print(list_1)
+print()
