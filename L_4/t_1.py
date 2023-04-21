eacdc0779c349548e844ed6a6b2ac0083e237927
Aleksandr Parhomenko <whitera_@mail.ru>
2023-04-21 13:00:16 +1000

    Задача 22

--- L_4/t_1.py
+++ L_4/t_1.py
@@ -1,29 +1,23 @@
 n = int(input("Ввод числа N - "))
-# m = int(input("Ввод числа M - "))
-list_0 = set()
-# list_1 = set()
+m = int(input("Ввод числа M - "))
+plenty_0 = set()
+plenty_1 = set()
 print()
 print(
     "ВНИМАНИЕ ! В данном коде используются множества которые не могую хранить повторяющиеся элементы !"
 )
 print("Числа не должны повторятся !")
 print()
+
 for i in range(n):
-    a = list_0.add(int(input("Ввод числа первого множества - ")))
-    x = 0
-    if x == a:
-        print("Такое число уже есть")
-    else:
-        x += 1
-        print("Тута")
-        break
-
-
-print(*list_0)  # Звёздочка нужна для того чтоб вывод был без фигурных скобок
+    a = plenty_0.add(int(input("Ввод числа первого множества - ")))
+print(*plenty_0)  # Звёздочка нужна для того чтоб вывод был без фигурных скобок
 print()
-"""
+
 for j in range(m):
-    list_1.add(int(input("Ввод числа второго множества - ")))
-print(*list_1)
+    plenty_1.add(int(input("Ввод числа второго множества - ")))
+print(*plenty_1)
 print()
-"""
+
+list_0 = sorted(plenty_0.union(plenty_1))
+print(*list_0)
---
+++ main.py
@@ -0,0 +1,3 @@
+list_1 = [2, 2, 2, 2]
+for i in list_1:
+    print(i)
