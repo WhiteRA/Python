3d4086102d6aec1321ef4cc786ade4b4daa07ea3
Aleksandr Parhomenko <whitera_@mail.ru>
2023-04-21 15:46:00 +1000

    Задача 24

---
+++ L_4/t_2.py
@@ -0,0 +1,17 @@
+from random import randint
+
+list_0 = []
+q = int(input("Колличество кустов - "))
+print()
+for i in range(q):
+    list_0.append(randint(0, 100))
+print(list_0)
+print()
+w = int(input("Номер куста - "))
+sum = 0
+
+if w > q:
+    print("Такого куста нет")
+else:
+    sum = list_0[w] + list_0[w - 1] + list_0[w + 1]
+    print(f"Колличество ягод - {sum}")
--- main.py
+++
@@ -1,3 +0,0 @@
-list_1 = [2, 2, 2, 2]
-for i in list_1:
-    print(i)
