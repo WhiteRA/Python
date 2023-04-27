c17d32edd069d04acbbbedf006c18a728fc6a668
Aleksandr Parhomenko <whitera_@mail.ru>
2023-04-27 16:05:05 +1000

    ДЗ

---
+++ L_5/t_1.py
@@ -0,0 +1,12 @@
+a = int(input("Число - "))
+b = int(input("Степень - "))
+
+
+def level(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / (a * level(a, -b - 1))
+
+
+print(level(a, b))
