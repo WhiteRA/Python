0c455674fe4bd1d11f454e461d818cfb77d9cd21
Aleksandr Parhomenko <whitera_@mail.ru>
2023-04-28 16:23:33 +1000

    Причёс

--- L_6/t_2.py
+++ L_6/t_2.py
@@ -1,10 +1,12 @@
 import random
 
-
-list_1 = [random.randint(-5, 20) for i in range(20)]
+list_1 = [random.randint(-5, 10) for i in range(10)]
 print(*list_1)
 print()
-min = int(input("MIN - "))
-max = int(input("MAX - "))
-list_2 = [i for i in range(20) if list_1[i] >= min and list_1[i] <= max]
-print(f"Номера элементов в заданном диапазоне - {list_2}")
+down = int(input("MIN - "))
+up = int(input("MAX - "))
+if down >= min(list_1) and up <= max(list_1):
+    list_2 = [i for i in range(20) if list_1[i] >= down and list_1[i] <= up]
+    print(f"Номера элементов в заданном диапазоне - {list_2}")
+else:
+    print("Введено значение выходящее за список")
